53509154077ac75b6f39a791399878865e61ebe1
Yadkee <yadkee@gmail.com>
2018-08-05 11:21:41 +0200

    parsing PES

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ PFMT = ("\033[0;44m[%04d]\033[42m(%02d)\033[0m %d|%d|%d %d|%d "
 RFMT = "\033[1;31m%s\033[0m"
 S = " " * 3
 PACKET_SIZE = 184 * 8  # In bits
+PES_WITH_EXTENSION = set([0xBD])
+PES_WITH_EXTENSION.update(range(0xC0, 0xDF + 1))
+PES_WITH_EXTENSION.update(range(0xE0, 0xEF + 1))
 
 PES_MASK = 0b000000000000000000000001
 MIP_MASK = 0b010001111110000000001111
@@ -37,8 +40,22 @@ def read_descriptors(stream):
     return out
 
 
-def tprint(n, s, width=3):
-    print(" " * n * width + s)
+def read_timestamp(stream):
+    ts = read_uint(stream, 3) << 30
+    stream.read(bool, 1)
+    ts += read_uint(stream, 15) << 15
+    stream.read(bool, 1)
+    ts += read_uint(stream, 15)
+    stream.read(bool, 1)
+    return ts
+
+
+def read_based_timestamp(stream):
+    stream.read(bool, 2)
+    base = read_timestamp(stream)
+    extension = read_uint(stream, 9)
+    stream.read(bool, 1)
+    return base * 300 + extension
 
 
 class Stream():
@@ -49,6 +66,8 @@ class Stream():
 
     def parse(self):
         self.pat = {}
+        self.pes = {}
+        pat = self.pat
         data = BitStream(self.raw)
         ignorePids = self.ignorePids
         onlyPusi = self.onlyPusi
@@ -85,6 +104,10 @@ class Stream():
             self.cPusi = pusi
             self.cAdaptationF = adaptationF
             self.cPayloadF = payloadF
+            try:
+                self.cProgram = pat[pid]
+            except KeyError:
+                self.cProgram = -1
             self.parse_packet(left)
 
     def parse_packet(self, data):
@@ -102,59 +125,50 @@ class Stream():
     def parse_adaptation(self, data, n=0):
         (discontinuity, rai, streamPriority, pcrF, opcrF, spliceF,
          privateF, extensionF) = data.read(bool, 8)
-        tprint(n, "ADAPTATION (%d)" % (len(data) // 8 + 1))
-        tprint(n, "FLAGS: %d|%d|%d|%d|%d|%d|%d|%d" % (discontinuity, rai,
+        print(S * n + "ADAPTATION (%d)" % (len(data) // 8 + 1))
+        print(S * n + "FLAGS: %d|%d|%d|%d|%d|%d|%d|%d" % (discontinuity, rai,
                streamPriority, pcrF, opcrF, spliceF, privateF, extensionF))
-        for i in range(pcrF + opcrF):
-            base = read_uint(data, 33)
-            _ = data.read(bool, 6)
-            extension = read_uint(data, 9)
-            if pcrF and not i:
-                pcr = base * 300 + extension
-                tprint(n, "PCR -> %d" % pcr)
-            else:
-                opcr = base * 300 + extension
-                tprint(n, "OPCR -> %d" % opcr)
+        pcr = opcr = 0
+        if pcrF:
+            pcr = read_based_timestamp(data)
+            print(S + "PCR -> %d" % pcr)
+            if opcrF:
+                opcr = read_based_timestamp(data)
+                print(S * n + "OPCR -> %d" % opcr)
         if spliceF:
             splice = data.read(int8)
-            tprint(n, "Splice Countdown: %d" % splice)
         if privateF:
             length = data.read(uint8)
             private = data.read(bytes, length)
-            tprint(n, "Private: %s" % private)
         if extensionF:
             length = data.read(uint8)
             extension = data.read(BitStream, length * 8)
             ltwF, pieceWiseF, seamlessF = extension.read(bool, 3)
-            tprint(n + 1, "EXTENSION (%d)" % length)
-            tprint(n + 1, "FLAGS: %d|%d|%d" % (ltwF, pieceWiseF, seamlessF))
+            print(S * n + "EXTENSION (%d) %d|%d|%d" %
+                  (length, ltwF, pieceWiseF, seamlessF))
             extension.read(bool, 5)
             if ltwF:
                 valid = extension.read(bool, 1)
                 offset = extension.read(BitStream, 15)
-                tprint(n + 1, "LTW %svalid offset: %s" %
-                       (["in", ""][valid], offset))
             if pieceWiseF:
                 extension.read(bool, 2)
                 rate = read_uint(extension, 22)
-                tprint(n + 1, "Piecewise rate: %d" % rate)
             if seamlessF:
                 spliceType = read_uint(extension, 4)
-                nextDts = read_uint(extension, 36) & 0x0efffefffe
-                tprint(n + 1, "Type: %d Next DTS: %d" % (spliceType, nextDts))
+                nextDts = read_timestamp(extension)
             if extension:
                 raise Exception("Bits left in extension: %s" % extension)
         if data:
             length = len(data)
             stuffing = data.read(bool, length)
             if all(stuffing):
-                tprint(n, "**%d Stuffing Bytes" % (length // 8))
+                print(S + "**%d Stuffing Bytes" % (length // 8))
             else:
                 raise Exception("Bits left in adaptation: %s" %
                                 "".join("01"[i] for i in stuffing))
 
     def parse_payload(self, data, n=0):
-        tprint(n, "PAYLOAD (%03d)" % (len(data) // 8))
+        print(S * n + "PAYLOAD (%03d)" % (len(data) // 8))
         if self.cPusi:
             first = read_uint(data.copy(24), 24)
             if first == PES_MASK:
@@ -167,11 +181,61 @@ class Stream():
     def parse_PES(self, data, n=0):
         data.read(bytes, 3)
         streamId = data.read(uint8)
-        length = read_uint(data, 16)
-        tprint(n, "PES[%03d](%d)" % (streamId, length))
-        if length > len(data) // 8:
-            length = len(data) // 8
-            self.cIncomplete = True
+        pesLength = read_uint(data, 16)
+        hasExtension = streamId in PES_WITH_EXTENSION
+        print(S * n + "PES[%03d](%d) |%d|" % (streamId, pesLength, hasExtension))
+        if hasExtension:
+            data.read(bool, 2)
+            scrambling = read_uint(data, 2)
+            (priority, alignment, copyrighted, original, ptsF, dtsF, escrF,
+             esRateF, dsmTrickF, copyF, crcF, extensionF) = data.read(bool, 12)
+            length = data.read(uint8)
+            print(S * n + "HEADER(%d) %d|%d|%d|%d %d|%d|%d|%d|%d|%d|%d|%d" %
+                  (length, priority, alignment, copyrighted, original, ptsF,
+                   dtsF, escrF, esRateF, dsmTrickF, copyF, crcF, extensionF))
+            header = data.read(BitStream, length * 8)
+            if ptsF:
+                header.read(bool, 4)
+                pts = read_timestamp(header)
+                print(S * n + "PTS -> %d" % pts)
+                if dtsF:
+                    header.read(bool, 4)
+                    dts = read_timestamp(header)
+                    print(S * n + "DTS -> %d" % dts)
+            if escrF:
+                header.read(bool, 2)
+                escr = read_based_timestamp(header)
+                print(S * n + "ESCR -> %d" % escr)
+            if esRateF:
+                header.read(bool)
+                rate = read_uint(header, 22)
+                header.read(bool)
+            if dsmTrickF:
+                dsmTrick = header.read(uint8)
+            if copyF:
+                header.read(bool)
+                copy = read_uint(header, 7)
+            if crcF:
+                crc = read_uint(header, 16)
+            if extensionF:
+                (privateF, fieldF, counterF,
+                 pstdF, _. _. _, extension2F) = header.read(bool, 8)
+                if privateF:
+                    private = header.read(bytes, 16)
+                if fieldF:
+                    field = header.read(uint8)
+                if counterF:
+                    header.read(bool)
+                    counter = read_uint(header, 7)
+                    header.read(bytes)
+                if pstdF:
+                    scale = [128, 1024][header.read(bool)]
+                    bufferSize = read_uint(header, 13) * scale
+                if extension2F:
+                    header.read(bool)
+                    fieldLength = read_uint(header, 7)
+                    header.read(bytes)
+
 
     def parse_PSI(self, data, n=0):
         data.read(bytes, data.read(uint8))
@@ -180,8 +244,8 @@ class Stream():
         syntaxF, privateBitF = data.read(bool, 2)
         data.read(bool, 2)
         length = read_uint(data, 12)
-        tprint(n, "PSI[%03d](%d) %d|%d" %
-               (tableId, length, syntaxF, privateBitF))
+        print(S * n + "PSI[%03d](%d) %d|%d" %
+              (tableId, length, syntaxF, privateBitF))
         if length > len(data) // 8:
             length = len(data) // 8
             self.cIncomplete = True
@@ -194,31 +258,31 @@ class Stream():
             currentF = syntax.read(bool)
             section, last = syntax.read(uint8, 2)
             tableData = syntax.read(BitStream, len(syntax) - 32)
-            tprint(n, "Syntax[%03d](%d) v%d |%d| %d/%d" %
-                   (tableIdExtension, len(tableData) // 8, version,
-                    currentF, section, last))
+            print(S * n + "Syntax[%03d](%d) v%d |%d| %d/%d" %
+                  (tableIdExtension, len(tableData) // 8, version,
+                   currentF, section, last))
             originalCrc = read_uint(syntax, 32)
             myCrc = crc32(copy.read(bytes, len(copy) // 8))
             if originalCrc != myCrc:
                 text = ("CRC does not match (0x%x vs 0x%x)" %
                         (originalCrc, myCrc))
-                tprint(n, RFMT % text)
+                print(S * n + RFMT % text)
             if not (self.cPid or tableId or privateBitF):  # PAT
                 while tableData:
                     programNum = read_uint(tableData, 16)
                     tableData.read(bool, 3)
                     programPid = read_uint(tableData, 13)
-                    tprint(n + 1, "PAT %d - %d" % (programNum, programPid))
+                    print(S * n + "PAT %d - %d" % (programNum, programPid))
                     if currentF:
                         self.pat[programPid] = programNum
-            elif self.cPid in self.pat and not privateBitF:  # PMT
+            elif self.cProgram >= 0 and not privateBitF:  # PMT
                 tableData.read(bool, 3)
                 pcrPid = read_uint(tableData, 13)
                 tableData.read(bool, 4)
                 length = read_uint(tableData, 12)
                 programDescriptors = tableData.read(BitStream, length * 8)
                 print(read_descriptors(programDescriptors))
-                tprint(n + 1, str(programDescriptors))
+                print(S * n + str(programDescriptors))
                 while tableData:
                     streamType = tableData.read(uint8)
                     tableData.read(bool, 3)
@@ -229,7 +293,7 @@ class Stream():
                           (elementatyPid, streamType, length, _length, pcrPid))
                     streamDescriptors = tableData.read(BitStream, _length * 8)
                     print(read_descriptors(streamDescriptors))
-                    tprint(n + 1, str(streamDescriptors))
+                    print(S * n + str(streamDescriptors))
             elif self.cPid == 17 and tableId == 66:  # SDT
                 networkId = read_uint(tableData, 16)
                 tableData.read(bytes, 1)
@@ -248,14 +312,14 @@ class Stream():
                     print(S * (n + 1) +
                           str(tableData.read(bytes, len(tableData) // 8)))
             else:
-                tprint(n + 1, RFMT % "Not registered")
+                print(S * n + RFMT % "Not registered")
             if tableData:
-                tprint(n, str(tableData))
+                print(S * n + str(tableData))
         if data:
             length = len(data)
             stuffing = data.read(bool, length)
             if all(stuffing):
-                tprint(n, "**%d Stuffing Bytes" % (length // 8))
+                print(S * n + "**%d Stuffing Bytes" % (length // 8))
             else:
                 return  # Only for debug purposes
                 raise Exception("Bits left in PSI: %s" %
@@ -277,4 +341,4 @@ def main(path, targetPids=None, ignorePids=tuple(), onlyPusi=False):
 if __name__ == "__main__":
     path = ("/home/huxley/Desktop/20180727-145000"
             "-20180727-145500-RGE1_CAT2_REC.ts")
-    main(path, onlyPusi=True, targetPids=range(2, 18))
+    main(path, onlyPusi=True)
