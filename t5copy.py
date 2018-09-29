b0563dad6fc65337c82088fce2ea90382cd9c598
Yadkee <yadkee@gmail.com>
2018-09-29 04:17:29 +0200

    Great advancements in t5 project

--- t5copy.py
+++ t5copy.py
@@ -1,6 +1,8 @@
 #! python3
 import iotools
 import tstools
+from subprocess import Popen
+from multiprocessing import Process
 
 """
 Process 0:
@@ -21,19 +23,46 @@ Process 2:
 RFMT = "\033[1m\033[91m%s\033[0m"
 
 
-
-
 def main(**kw):
-    if "targetPids" in kw:
-        skipPids = set(range(1 << 13)) - set(kw.pop("targetPids"))
-    else:
-        skipPids = set(kw.pop("skipPids", tuple()))
-    skipPids.add(8191)  # Stuffing packet
+    p = Process(target=listen, kwargs=kw)
+    p.start()
+    filter(**kw)
+
+
+def filter(**kw):
+    """Process 0"""
+    program = kw.pop("program")
+    # 1
+    for payload in tstools.store_PSI(targetPids=(0,), **kw):
+        pat = tstools.parse_PAT(payload)
+        break
+    reversedPat = dict((j, i) for i, j in pat.items())
+    pmtPid = reversedPat[program]
+    # 2
+    for payload in tstools.store_PSI(targetPids=(pmtPid,), **kw):
+        pmt = tstools.parse_PMT(payload)
+        break
+    p = ffmpeg(pmt, kw["path"])
+    # 3
+    for pid, pusi, pF, aF, packet in tstools.parsed_loop(targetPids=pmt, **kw):
+        pass
+
+
+def ffmpeg(pmt, inp):
+    """Process 1"""
+    maps = " ".join("-map i:%s" % hex(i) for i in pmt)
+    streams = " ".join("-streamid %d:%d" % (a, i) for a, i in enumerate(pmt))
+    line = ("ffmpeg -y -i %s %s %s -copy_unknown -c copy"
+            " save.ts") % (inp, maps, streams)  # TODO udp://localhost:9876
+    return Popen(line, shell=True)
+
+
+def listen(**kw):
+    """Process 2"""
+    pass
 
 
 if __name__ == "__main__":
     path = ("/home/huxley/Desktop/20180727-145000"
             "-20180727-145500-RGE1_CAT2_REC.ts")
-    path = ("C:/users/angel/Desktop/20180727-145000"
-            "-20180727-145500-RGE1_CAT2_REC.ts")
-    main(path=path)
+    main(path=path, program=498)
